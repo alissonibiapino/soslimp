696fd6310e6ac39898fe08f2b8b7289cc6d0776f
Alisson <alissoncf8@gmail.com>
2025-11-07 18:32:37 -0300

    Listagens das vendas nos últimos 7 dias

--- database/queries.py
+++ database/queries.py
@@ -9,19 +9,40 @@ def get_colaboradores():
     colaboradores = cur.fetchall()
     # colaboradores = [row[0] for row in cur.fetchall()]
     conn.close()
-    print(colaboradores)
+    # print(colaboradores)
     return colaboradores
 
-# QUERIES DE COLABORADOR
+
+# QUERIES DE PRODUTOS
 
 def get_produtos():
     conn = get_conn()
     cur = conn.cursor()
     cur.execute("SELECT id_produto, nome_produto, marca, preco_unitario FROM produto")
-    produtos = cur.fetchall()
-
-    # produtos_dict = {nome: id for }
-
+    resultado = cur.fetchall()
     conn.close()
-    print(produtos)
-    return produtos
+
+    produtos_dict = {
+        nome_produto: {"id":id, "marca":marca, "preco":preco}
+        for id, nome_produto, marca, preco in resultado
+    }
+    print(produtos_dict)
+    return produtos_dict
+
+
+# QUERIES DE VENDAS
+
+def get_ultimas_vendas():
+    conn = get_conn()
+    cur = conn.cursor()
+    cur.execute("SELECT id_reg_venda, forma_pagamento.tipo_pagamento, data_reg_venda, preco_unitario FROM registro_venda INNER JOIN forma_pagamento ON( registro_venda.cod_forma_pag = forma_pagamento.cod_forma_pag) WHERE data_reg_venda >= current_date - INTERVAL '7 days';")
+    resultado = cur.fetchall()
+    conn.close()
+
+    ultimas_vendas_dict = {
+        id_reg_venda: {"id":id_reg_venda, "forma_pagamento":tipo_pagamento, "data":data_reg_venda, "preco_unitario":preco_unitario}
+        for id_reg_venda, tipo_pagamento, data_reg_venda, preco_unitario in resultado
+    }
+
+    return ultimas_vendas_dict
+    
--- views/coluna3.py
+++ views/coluna3.py
@@ -1,11 +1,32 @@
 import customtkinter as ctk
 
-from database.queries import get_produtos
+from theme import colors
 
-produtos = get_produtos()
-# produtos_dict = {nome_produto:   in produtos}
+from database.queries import get_ultimas_vendas
+
+ultimas_vendas = get_ultimas_vendas()
+print(ultimas_vendas)
 
 class Coluna3(ctk.CTkFrame):
     def __init__(self, parent):
-        super().__init__(parent)
+        super().__init__(parent, fg_color=colors.BRANCO)
+
+        self.grid_columnconfigure(0, weight=1)
+
+        self.grid_rowconfigure(0, weight=1)
+
+        linha1 = ctk.CTkFrame(
+            self,
+            fg_color=colors.BRANCO,
+            corner_radius=10,
+            border_width=2,
+            border_color=colors.AZUL_SECUNDARIO)
         
+        linha1.grid(row=0, column=0, sticky="nsew", padx=10, pady=5)
+        linha1.grid_columnconfigure(0, weight=1)
+        linha1.grid_rowconfigure(0, weight=1)
+
+        for linha, (id_venda, dados) in enumerate(ultimas_vendas.items()):
+            label = ctk.CTkLabel(linha1, text=f"{dados['forma_pagamento']} — R${dados['preco_unitario']}")
+            label.grid(row=linha, column=0, padx=10, pady=5)
+
