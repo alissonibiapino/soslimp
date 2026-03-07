a6d05ee87e147fab92ba6287ad422585edbe1794
alissonibiapino <alissoncf8@gmail.com>
2026-03-07 16:54:04 -0300

    Organização das pastas

--- sessao.py
+++
@@ -1,9 +0,0 @@
-class SessaoDeLogin:
-    cod_colaborador = None
-    nome = None
-    cargo = None
-    loja_cod = None
-    loja_nome = None
-    loja_end = None
-    loja_num = None
-    loja_email = None
--- theme/fonts.py
+++
@@ -1,9 +0,0 @@
-import customtkinter as ctk
-
-FONTE_TITULO = None
-FONTE_TEXTO = None
-
-def iniciar_fontes():
-    global FONTE_TITULO
-    FONTE_TITULO = ctk.CTkFont(family="Segoe UI", size=22, weight='bold')
-    FONTE_TEXTO = ctk.CTkFont(family="Segoe UI", size=16)
