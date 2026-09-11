c50f6fa16b7ed60c59e4d548c1925f4b3c08222c
alissonibiapino <alissoncf8@gmail.com>
2026-09-11 20:50:47 -0300

    feat(routes): adiciona rota de editar estoque manualmente

--- backend/routes/estoque.py
+++ backend/routes/estoque.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Body
 from database.conn_postgres import get_conn
 
-from services.estoque_services import listar_estoque_por_loja, listar_estoque
+from services.estoque_services import listar_estoque_por_loja, listar_estoque, ajustar_estoque_manualmente
 
 router = APIRouter(prefix="/estoque", tags=["Estoque"])
 
@@ -12,3 +12,7 @@ def get_listar_estoque():
 @router.get("/{cod_loja}")
 def get_estoque_por_loja(cod_loja: int):
     return listar_estoque_por_loja(cod_loja)
+
+@router.post("/ajustar")
+def ajustar_estoque(ajuste: dict = Body(...)):
+    return ajustar_estoque_manualmente(ajuste)
--- backend/services/estoque_services.py
+++ backend/services/estoque_services.py
@@ -40,3 +40,27 @@ def listar_estoque_por_loja(cod_loja: int):
     finally:
         cur.close()
         conn.close()
+
+def ajustar_estoque_manualmente(ajuste):
+    conn = get_conn()
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+
+    print(ajuste)
+    print(ajuste["quantidade"])
+    print(ajuste["cod_produto"])
+    print(ajuste["cod_loja"])
+
+    try:
+        cur.execute("""
+            UPDATE estoque SET quantidade_atual = %s WHERE cod_produto = %s AND cod_loja = %s;
+        """, (ajuste["quantidade"], ajuste["cod_produto"], ajuste["cod_loja"]))
+        conn.commit()
+        return "Estoque ajustado com sucesso!"
+
+    except Exception as e:
+        conn.rollback()
+        return print(f"Erro no banco: {e}")
+    
+    finally:
+        cur.close()
+        conn.close()
